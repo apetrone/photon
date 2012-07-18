9bad32733caa7f2f67a9770205efe0968033916d
Adam Petrone <adampetrone83@gmail.com>
2012-07-17 22:38:05 -0400

    Initial commit of the first day of graphics course work. Barycentric Coordinates and rasterizing a triangle to an image. wee

---
+++ src/entry.cpp
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <string.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <SOIL.h>
+
+
+#define PVEC2( v ) fprintf( stdout, "" #v "= %g, %g\n", v[0], v[1] )
+#define PVEC3( v ) fprintf( stdout, "" #v "= %g, %g, %g\n", v[0], v[1], v[2] )
+#define PVEC4( v ) fprintf( stdout, "" #v "= %g, %g, %g, %g\n", v[0], v[1], v[2], v[3] )
+
+#define VIEWPORT_WIDTH 320.0f
+#define VIEWPORT_HEIGHT 240.0f
+
+// object coordinates * (model view matrix) = eye coordinates
+// eye coordinates * (projection matrix ) = clip coordinates
+// clip coordinates / (perspective divide) = normalized device coordinates
+// normalized device coordinates * viewport transform = screen coordinates
+
+// equation from wikipedia: http://en.wikipedia.org/wiki/Barycentric_coordinate_system_(mathematics)
+void barycentric_coords( const glm::vec2 & p, const glm::vec2 & a, const glm::vec2 & b, const glm::vec2 & c, float * alpha, float * beta, float * gamma )
+{
+	float detT = 1.0 / ((b.y - c.y) * (a.x - c.x) + (c.x - b.x) * (a.y - c.y));
+	*alpha = ((b.y - c.y)*(p.x - c.x) + (c.x - b.x) * (p.y - c.y)) * detT;
+	*beta = ((c.y - a.y)*(p.x - c.x) + (a.x - c.x) * (p.y - c.y)) * detT;
+
+	*gamma = 1.0f - *alpha - *beta;
+}
+
+struct Vertex
+{
+	glm::vec3 position;
+	float r, g, b, a;
+};
+
+int main( int argc, char ** argv )
+{
+	const int channels = 4;
+	unsigned char * pixels = new unsigned char[ (int)VIEWPORT_WIDTH * (int)VIEWPORT_HEIGHT * channels ];
+
+	// setup the triangle here	
+	Vertex vertices[3];
+	vertices[0].position = glm::vec3( -50, 0, 0 );
+	vertices[0].r = 1.0f; vertices[0].g = vertices[0].b = 0.0f; vertices[0].a = 1.0f;
+	vertices[1].position = glm::vec3( 50, 0, 0 );
+	vertices[1].r = 0.0f; vertices[1].g = 1.0f; vertices[1].b = 0.0f; vertices[1].a = 1.0f;
+	vertices[2].position = glm::vec3( 0, -50, 0 );
+	vertices[2].r = 0.0f; vertices[2].g = 0.0f; vertices[2].b = 1.0f; vertices[2].a = 1.0f;
+	
+	fprintf( stdout, "World Coordinates:\n" );
+	PVEC3( vertices[0].position );
+	PVEC3( vertices[1].position );
+	PVEC3( vertices[2].position );
+
+
+	glm::mat4 modelview = glm::mat4(1.0); // world to eye
+//	modelview = glm::translate( modelview, glm::vec3( 0, 0, 150 ));
+
+	glm::mat4 projection; // eye to clip
+	projection = glm::ortho( -VIEWPORT_WIDTH/2.0f, VIEWPORT_WIDTH/2.0f, VIEWPORT_HEIGHT/2.0f, -VIEWPORT_HEIGHT/2.0f, -0.1f, 128.0f );
+	glm::mat4 rotate = glm::rotate( glm::mat4(1.0), 45.0f, glm::vec3(0,1,0) );
+	
+	// origin in the top left
+//	projection = glm::ortho( 0.0f, (float)VIEWPORT_WIDTH, (float)VIEWPORT_HEIGHT, 0.0f, -0.1f, 128.0f );
+//	projection = glm::perspective( 60.0f, ((float)VIEWPORT_WIDTH/(float)VIEWPORT_HEIGHT), 0.1f, 128.0f );
+
+	// convert triangle to clip space
+	glm::mat4 world_to_clip = (projection * modelview);
+	glm::vec4 a = world_to_clip * glm::vec4( vertices[0].position, 1.0 );
+	glm::vec4 b = world_to_clip * glm::vec4( vertices[1].position, 1.0 );
+	glm::vec4 c = world_to_clip * glm::vec4( vertices[2].position, 1.0 );
+
+	fprintf( stdout, "Converted to Clip Coordinates:\n" );
+	PVEC4(a);
+	PVEC4(b);
+	PVEC4(c);	
+	
+	fprintf( stdout, "Converted to Normalized Device Coordinates:\n" );
+	// convert to normalized device coordinates
+	a *= (1/a.w);
+	b *= (1/b.w);
+	c *= (1/c.w);
+
+	PVEC4(a);
+	PVEC4(b);
+	PVEC4(c);
+
+	fprintf( stdout, "Converted to Screen Coordinates:\n" );
+
+	//http://stackoverflow.com/questions/8491247/c-opengl-convert-world-coords-to-screen2d-coords
+	// convert triangle to screen coordinates
+
+	glm::vec2 viewOffset( 0, 0 );
+	glm::vec2 viewSize( VIEWPORT_WIDTH, VIEWPORT_HEIGHT );
+	
+	glm::vec2 wa = glm::vec2(
+	wa[0] = (a[0]+1.0) / 2.0 * viewSize[0] + viewOffset[0],
+	wa[1] = (a[1]+1.0) / 2.0 * viewSize[1] + viewOffset[1] );
+
+	glm::vec2 wb = glm::vec2(
+	wb[0] = (b[0]+1.0) / 2.0 * viewSize[0] + viewOffset[0],
+	wb[1] = (b[1]+1.0) / 2.0 * viewSize[1] + viewOffset[1] );
+
+	glm::vec2 wc = glm::vec2(
+	wc[0] = (c[0]+1.0) / 2.0 * viewSize[0] + viewOffset[0],
+	wc[1] = (c[1]+1.0) / 2.0 * viewSize[1] + viewOffset[1] );
+
+	PVEC2(wa);
+	PVEC2(wb);
+	PVEC2(wc);
+
+	// fill image with a color
+	unsigned char * p = pixels;
+	// I invert the direction that the height is traversed to match the same image output as OpenGL
+	// Alternatively, I could simply flip the image when I'm done...
+	for( int h = VIEWPORT_HEIGHT; h > 0; --h )
+//	for( int h = 0; h < VIEWPORT_HEIGHT; ++h )
+	{
+		for( int w = 0; w < VIEWPORT_WIDTH; ++w )
+		{
+			float alpha, beta, gamma;
+			
+			// use the pixel center
+			glm::vec2 ndc = glm::vec2(w + 0.5f, h + 0.5f);
+			
+			if ( 1 ) // normalized device coordinate space
+			{
+				// convert screen coords back to normalized device coordinates
+				ndc.x = ((ndc.x - viewOffset[0]) / (viewSize[0])) * 2.0 - 1.0;
+				ndc.y = ((ndc.y - viewOffset[1]) / (viewSize[1])) * 2.0 - 1.0;
+
+				// calculate barycentric coordinates
+				barycentric_coords( ndc, glm::vec2(a), glm::vec2(b), glm::vec2(c), &alpha, &beta, &gamma );
+			}
+
+			else if ( 1 ) // screen space
+			{			
+				// calculate barycentric coordinates
+				barycentric_coords( ndc, wa, wb, wc, &alpha, &beta, &gamma );
+			}
+			
+			// determine if this pixel resides in the triangle or not
+			if ( alpha > 0 && alpha < 1 && beta > 0 && beta < 1 && gamma > 0 && gamma < 1 )
+			{
+				// interpolate the color
+				p[0] = (vertices[0].r * alpha + vertices[1].r * beta + vertices[2].r * gamma) * 255.0f;
+				p[1] = (vertices[0].g * alpha + vertices[1].g * beta + vertices[2].g * gamma) * 255.0f;
+				p[2] = (vertices[0].b * alpha + vertices[1].b * beta + vertices[2].b * gamma) * 255.0f;
+				p[3] = (vertices[0].a * alpha + vertices[1].a * beta + vertices[2].a * gamma) * 255.0f;
+			}
+			else
+			{
+				p[0] = 32;
+				p[1] = 32;
+				p[2] = 32;
+				p[3] = 255;
+			}
+
+			p += channels;
+		}
+	}
+
+
+	int save_result = SOIL_save_image( "output/output.bmp", SOIL_SAVE_TYPE_BMP, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, channels, pixels );
+	if ( save_result != 1 )
+	{
+		fprintf( stdout, "saving to output failed!\n" );
+	}
+
+	delete [] pixels;
+
+	return 0;
+}
